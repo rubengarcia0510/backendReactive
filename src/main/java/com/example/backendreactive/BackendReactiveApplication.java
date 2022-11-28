e3de675425261300bc505e70f5067e87c30a4338
Ruben Garcia <rubengarcia0510@gmail.com>
2022-11-27 22:53:40 -0300

    first commit

---
+++ src/main/java/com/example/backendreactive/BackendReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendReactiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendReactiveApplication.class, args);
+    }
+
+}
