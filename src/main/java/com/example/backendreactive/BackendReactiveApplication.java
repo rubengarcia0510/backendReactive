7d1d4d867d0598b3ca1a1aa30b34ad3cc079bcc5
Ruben Garcia <rubengarcia0510@gmail.com>
2022-12-03 19:51:27 -0300

    @EnableWebFlux

--- src/main/java/com/example/backendreactive/BackendReactiveApplication.java
+++ src/main/java/com/example/backendreactive/BackendReactiveApplication.java
@@ -2,12 +2,17 @@ package com.example.backendreactive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class BackendReactiveApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BackendReactiveApplication.class, args);
+
+
     }
 
 }
