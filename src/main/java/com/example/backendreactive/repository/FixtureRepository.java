2890490dc4910b91b64ad0ca7f052b90f3aeae8f
Ruben Garcia <rubengarcia0510@gmail.com>
2022-12-08 13:56:22 -0300

    add FixtureRepository

--- src/main/java/com/example/backendreactive/repository/FixtureRepository.java
+++ src/main/java/com/example/backendreactive/repository/FixtureRepository.java
@@ -1,2 +1,7 @@
-package com.example.backendreactive.repository;public interface FixtureRepository {
+package com.example.backendreactive.repository;
+
+import com.example.backendreactive.model.Fixture;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface FixtureRepository extends MongoRepository<Fixture, String> {
 }
