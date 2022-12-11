c6137fb452c23e66271db150c077e0d5c2fbdc7b
Ruben Garcia <rubengarcia0510@gmail.com>
2022-12-11 13:04:02 -0300

    add CrossOrigin

--- src/main/java/com/example/backendreactive/controller/GroupController.java
+++ src/main/java/com/example/backendreactive/controller/GroupController.java
@@ -8,10 +8,7 @@ import com.example.backendreactive.repository.GroupRepository;
 import com.example.backendreactive.repository.GroupsRepository;
 import com.example.backendreactive.service.GroupsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -25,6 +22,7 @@ public class GroupController {
     @Autowired
     private GroupsService groupsService;
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("/{id}")
     private Mono<Group> getGroup(@PathVariable String id){
         return groupsService.getGroupByName(id).log();
@@ -36,6 +34,7 @@ public class GroupController {
         return groupRepository.getAllGroups();
     }
 
+    @CrossOrigin(origins = "http://localhost:8080")
     @GetMapping("all")
     private Flux<Groups> findAllGroups(){
         return groupsService.getAllGroups();
